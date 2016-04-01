f33eda72176fd58e819693e67a5dc3838f413987
App Academy Student <student@appacademy.io>
2016-04-01 14:40:13 -0700

    created subs table and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-  
+
+  resources :subs, except: :destroy
+
   root 'users#new'
 end
